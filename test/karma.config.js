be9e405cbda8f3d5e6470e6ac1c1cc126a2bdc95
Manuel Puyol <manuelpuyol@github.com>
2025-03-17 09:50:58 -0700

    use chrome --no-sandbox

--- test/karma.config.js
+++ test/karma.config.js
@@ -34,7 +34,13 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: ['--no-sandbox']
+      }
+    },
     autoWatch: false,
     singleRun: true,
     concurrency: Infinity,
